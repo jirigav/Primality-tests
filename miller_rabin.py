bc03c6556c9f2b048387c0f8ede867afabf24c5c
jirigav <jirigavenda98@gmail.com>
2019-09-07 22:41:29 +0200

    naming change and algorithm corrected

--- miller_rabin.py
+++ miller_rabin.py
@@ -1,26 +1,24 @@
 from random import randint
 
 
-def is_probably_prime(n, loops):
-    if n < 3:
-        return n == 2
+def is_probably_prime(number, loops):
+    if number < 4:
+        return number == 2 or number == 3
 
-    d = n - 1
+    d = number - 1
     s = 0
     while not d % 2:
         s += 1
         d //= 2
     for i in range(loops):
-        a = randint(2, n- 1)
-        x = pow(a, d, n)
+        a = randint(2, number - 2)
+        x = pow(a, d, number)
 
-        if x == 1 or x == (n - 1):
+        if x == 1 or x == (number - 1):
             continue
         for _ in range(s - 1):
-            x = pow(x, 2, n)
-            if x == 1:
-                return False
-            if x == (n - 1):
+            x = pow(x, 2, number)
+            if x == (number - 1):
                 break
         else:
             return False
